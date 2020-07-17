61ae78b29d465cf8b2c57e918de8ea6fbf9ea220
Jay <jay1999ke@gmail.com>
2020-07-17 23:35:15 +0530

    Use Stochastic predictions in MACD

--- trader/macd.py
+++ trader/macd.py
@@ -5,15 +5,24 @@ sys.path.insert(0, myPath + '/../')
 import torch
 import  matplotlib.pyplot as plt
 import numpy as np
+
 from data.loader import cryptoData
+from models.model import  MLPRegressor
 
 DEVICE = torch.device("cpu")
+MODEL = "norm"
 
 # Function for MACD Strategy
 
 def macd(coin_name, amount):
+
+    model = MLPRegressor(coin=coin_name.lower(),model= MODEL)
+    model.to(DEVICE)
+
     dataloaderX = cryptoData(coin_name.lower(),DEVICE=DEVICE)
-    
+    DAYS = len(dataloaderX)
+    model.eval(dataloaderX[0][0])
+
     macd_line = []
     signal_line = []
     x = []
@@ -22,8 +31,21 @@ def macd(coin_name, amount):
     no_of_coins = 0
     print("\n",coin_name.upper(),":")
     
-    for i in range(816): # TODO: Discuss standard time slot across all algorithms
+    for i,(x_input,target) in enumerate(dataloaderX): # TODO: Discuss standard time slot across all algorithms
+        if i < 34: # let dataloader catchup with macd range
+            continue
+        if i == DAYS:
+            break
+    
+        predictedPrice = model(x_input) * dataloaderX.pmax
+
         (macd, macd_9, ema_12, ema_26, price) = dataloaderX.getMacDData(i)
+
+        ema_12 = ema_12 + (predictedPrice - ema_12)*dataloaderX.multiplier_12
+        ema_26 = ema_26 + (predictedPrice - ema_26)*dataloaderX.multiplier_26
+        macd = ema_12 - ema_26
+        macd_9 = macd_9 + (macd - macd_9)*dataloaderX.multiplier_9
+
 
         x.append(i)
         macd_line.append(macd)
